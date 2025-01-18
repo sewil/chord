53df94980c9be0652be093bc081c71502b2b4324
sewil <1498414+sewil@users.noreply.github.com>
2025-01-18 21:25:12 +0100

    Handle RhythmVerse empty result

--- Core/API/RhythmVerse/Models/SearchResult.cs
+++ Core/API/RhythmVerse/Models/SearchResult.cs
@@ -10,9 +10,26 @@ namespace Chord.Core.API.RhythmVerse.Models
         [JsonProperty("status")]
         public string Status { get; set; }
     }
+    public class SongsConverter : JsonConverter<Song[]>
+    {
+        public override Song[] ReadJson(JsonReader reader, Type objectType, Song[] existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Boolean && reader.Value is bool boolean && !boolean)
+            {
+                return Array.Empty<Song>();
+            }
+            return serializer.Deserialize<Song[]>(reader);
+        }
+
+        public override void WriteJson(JsonWriter writer, Song[] value, JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, value);
+        }
+    }
     public class SearchResultData
     {
         [JsonProperty("songs")]
+        [JsonConverter(typeof(SongsConverter))]
         public Song[] Songs { get; set; }
     }
     public class Song
